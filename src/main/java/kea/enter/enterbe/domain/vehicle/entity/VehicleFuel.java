ba40506b79ca27b35ab36f6ee6902e1397dddc29
SeohyunSim <angelsh0805@gachon.ac.kr>
2024-07-21 16:52:45 +0900

    Feat : 공통 Entity 생성 (Vehicle, VehicleNote, Winning) (#4)
     Feat: Vehicle, VehicleNote, Winning Entity 생성
     #4

---
+++ src/main/java/kea/enter/enterbe/domain/vehicle/entity/VehicleFuel.java
@@ -0,0 +1,7 @@
+package kea.enter.enterbe.domain.vehicle.entity;
+
+public enum VehicleFuel {
+    DIESEL,
+    GASOLINE,
+    ELECTRICITY
+}
---
+++ src/main/java/kea/enter/enterbe/domain/vehicle/entity/VehicleState.java
@@ -0,0 +1,11 @@
+package kea.enter.enterbe.domain.vehicle.entity;
+
+public enum VehicleState {
+    AVAILABLE,
+    WAIT_TAKE,
+    TAKE_COMPLETE,
+    ON_RENT,
+    WAIT_RETURN,
+    RETURN_COMPLETE,
+    INACTIVE
+}
