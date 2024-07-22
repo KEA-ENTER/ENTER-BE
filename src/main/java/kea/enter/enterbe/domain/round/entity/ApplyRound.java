7e91df27a5e9e84b57662b0b216084b06fcd6162
이민기 <mingy837@gmail.com>
2024-07-22 19:47:29 +0900

    Feat : ApplyRound Entity 생성 (#6) - ApplyRound.java / ApplyRoundState.java 생성 #6

---
+++ src/main/java/kea/enter/enterbe/domain/round/entity/ApplyRound.java
@@ -0,0 +1,48 @@
+package kea.enter.enterbe.domain.round.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import kea.enter.enterbe.domain.vehicle.entity.Vehicle;
+import kea.enter.enterbe.global.common.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="apply_round")
+public class ApplyRound extends BaseEntity {
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="vehicle_id", nullable = false)
+    private Vehicle vehicleId;
+
+    @Column(name="applu_round", nullable = false)
+    private Integer applyRound;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="state", nullable = false)
+    private ApplyRoundState state;
+
+    @Builder
+    public ApplyRound(Vehicle vehicleId, Integer applyRound, ApplyRoundState state){
+        this.vehicleId = vehicleId;
+        this.applyRound = applyRound;
+        this.state = state;
+    }
+
+    public static ApplyRound of(Vehicle vehicleId, Integer applyRound, ApplyRoundState state){
+        return ApplyRound.builder()
+            .vehicleId(vehicleId)
+            .applyRound(applyRound)
+            .state(state)
+            .build();
+    }
+}
