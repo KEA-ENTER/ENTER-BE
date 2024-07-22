16e533a0e43994b2a477a18e3535d4941277e6de
이민기 <mingy837@gmail.com>
2024-07-22 19:41:55 +0900

    Feat : Apply Entity 생성(#6) - Apply.java / ApplyState.java 생성 #6

---
+++ src/main/java/kea/enter/enterbe/domain/apply/entity/Apply.java
@@ -0,0 +1,69 @@
+package kea.enter.enterbe.domain.apply.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import kea.enter.enterbe.domain.member.entity.Member;
+import kea.enter.enterbe.domain.round.entity.ApplyRound;
+import kea.enter.enterbe.global.common.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "apply")
+public class Apply extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id", nullable = false)
+    private Member memberId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="apply_round_id", nullable = false)
+    private ApplyRound applyRoundId;
+
+    @Column(name="vehicle_id", nullable = false)
+    private Long vehicleId;
+
+    @Column(name="departures", nullable = false)
+    private String departures;
+
+    @Column(name="arrivals", nullable = false)
+    private String arrivals;
+    @Enumerated(EnumType.STRING)
+    @Column(name="state", nullable = false)
+    private ApplyState state;
+    @Builder
+    public Apply(Member memberId, ApplyRound applyRoundId, Long vehicleId, String departures,
+        String arrivals, ApplyState state){
+        this.memberId = memberId;
+        this.applyRoundId = applyRoundId;
+        this.vehicleId = vehicleId;
+        this.departures = departures;
+        this.arrivals = arrivals;
+        this.state = state;
+    }
+
+    public static Apply of(Member memberId, ApplyRound applyRoundId, Long vehicleId, String departures,
+        String arrivals, ApplyState state){
+        return Apply.builder()
+            .memberId(memberId)
+            .applyRoundId(applyRoundId)
+            .vehicleId(vehicleId)
+            .departures(departures)
+            .arrivals(arrivals)
+            .state(state)
+            .build();
+    }
+
+
+}
