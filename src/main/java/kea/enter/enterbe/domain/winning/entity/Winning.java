11758f55a3442d8ec2240d5b4b9afa3b973e318c
Dongseok Kang <dsk08208@gmail.com>
2024-07-23 13:11:48 +0900

    Fix : 공통 Entity 수정 (Vehicle, Winning) (#25)
    - Vehicle Entity 명명 수정
    - Winning Entity 명명 수정
    
    공통 Entity 수정 (Vehicle, Winning) #4
    
    Co-authored-by: SeohyunSim <angelsh0805@gachon.ac.kr>

--- src/main/java/kea/enter/enterbe/domain/winning/entity/Winning.java
+++ src/main/java/kea/enter/enterbe/domain/winning/entity/Winning.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import kea.enter.enterbe.domain.apply.entity.Apply;
 import kea.enter.enterbe.domain.vehicle.entity.Vehicle;
 import kea.enter.enterbe.global.common.entity.BaseEntity;
 import lombok.AccessLevel;
@@ -24,24 +25,22 @@ public class Winning extends BaseEntity {
     @JoinColumn(name = "vehicle_id", nullable = false)
     private Vehicle vehicle;
 
-    // 추후 수정예정
-    @Column(name = "apply_id", nullable = false)
-    private Integer apply;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "apply_id", nullable = false)
+    private Apply apply;
 
     @Column(name = "state", nullable = false)
     @Enumerated(EnumType.STRING)
     private WinningState state;
 
-    // apply 수정에 따라 추후 수정예정
     @Builder
-    public Winning(Vehicle vehicle, Integer apply, WinningState state) {
+    public Winning(Vehicle vehicle, Apply apply, WinningState state) {
         this.vehicle = vehicle;
         this.apply = apply;
         this.state = state;
     }
 
-    // apply 수정에 따라 추후 수정예정
-    public static Winning of(Vehicle vehicle, Integer apply, WinningState state) {
+    public static Winning of(Vehicle vehicle, Apply apply, WinningState state) {
         return Winning.builder().vehicle(vehicle).apply(apply).state(state).build();
     }
 }
