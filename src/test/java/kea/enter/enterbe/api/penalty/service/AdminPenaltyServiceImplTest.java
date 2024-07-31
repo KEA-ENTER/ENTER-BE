548881b28fcf57a17340992c3385425561fa4db1
joeun-01 <jjy020121@gmail.com>
2024-07-31 15:46:40 +0900

    Feat: 페널티 삭제 API 테스트 코드 - 페널티 삭제 API 테스트 코드 작성  #40

--- src/test/java/kea/enter/enterbe/api/penalty/service/AdminPenaltyServiceImplTest.java
+++ src/test/java/kea/enter/enterbe/api/penalty/service/AdminPenaltyServiceImplTest.java
@@ -2,6 +2,7 @@ package kea.enter.enterbe.api.penalty.service;
 
 import kea.enter.enterbe.IntegrationTestSupport;
 import kea.enter.enterbe.api.penalty.controller.response.GetPenaltyListResponse;
+import kea.enter.enterbe.api.penalty.service.dto.DeletePenaltyServiceDto;
 import kea.enter.enterbe.api.penalty.service.dto.GetPenaltyListServiceDto;
 import kea.enter.enterbe.api.penalty.service.dto.PostPenaltyServiceDto;
 import kea.enter.enterbe.domain.member.entity.Member;
@@ -14,6 +15,7 @@ import kea.enter.enterbe.domain.penalty.entity.PenaltyState;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
+import java.util.Optional;
 
 import static kea.enter.enterbe.domain.penalty.entity.PenaltyLevel.BLACKLIST;
 import static kea.enter.enterbe.domain.penalty.entity.PenaltyReason.BROKEN;
@@ -23,7 +25,7 @@ import static org.assertj.core.api.Assertions.tuple;
 class AdminPenaltyServiceImplTest extends IntegrationTestSupport {
     @DisplayName(value = "사용자 페널티를 생성한다")
     @Test
-    public void postPenalty() throws Exception {
+    public void postPenalty() {
         //given
         Member member = memberRepository.save(createMember());
         PostPenaltyServiceDto dto = PostPenaltyServiceDto.of(member.getId(), PenaltyReason.BROKEN, PenaltyLevel.BLACKLIST, null);
@@ -42,7 +44,7 @@ class AdminPenaltyServiceImplTest extends IntegrationTestSupport {
 
     @DisplayName(value = "사용자의 페널티 목록을 조회한다.")
     @Test
-    public void getPenaltyList() throws Exception {
+    public void getPenaltyList() {
         //given
         Member member = memberRepository.save(createMember());
         Long memberId = member.getId();
@@ -65,6 +67,26 @@ class AdminPenaltyServiceImplTest extends IntegrationTestSupport {
                 tuple(PenaltyReason.BROKEN, PenaltyLevel.BLACKLIST),
                 tuple(PenaltyReason.BROKEN, PenaltyLevel.BLACKLIST)
             );
+    }
+
+    @DisplayName(value = "사용자의 페널티를 삭제한다.")
+    @Test
+    public void deletePenalty() {
+        //given
+        Member member = memberRepository.save(createMember());
+        Long memberId = member.getId();
+
+        Penalty penalty = penaltyRepository.save(createPenalty(member));
+        Long penaltyId = penalty.getId();
+
+        DeletePenaltyServiceDto dto = DeletePenaltyServiceDto.of(memberId, penaltyId);
+
+        //when
+        adminPenaltyService.deletePenalty(dto);
+
+        //then
+        Optional<Penalty> result = penaltyRepository.findByIdAndMemberIdAndState(penaltyId, memberId, PenaltyState.ACTIVE);
+        assertThat(result).isEmpty();
     }
 
     private Member createMember() {
