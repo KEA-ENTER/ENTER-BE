b5bc74d71258f43ad9c2444dbe8ab9e100d99120
정조은 <jjy020121@gmail.com>
2024-07-31 19:45:39 +0900

    페널티 삭제 API 구현 (#52)
    * Feat: 페널티 삭제 API
    - 페널티 삭제 API
     #40
    
    * Feat: 페널티 삭제 API 테스트 코드
    - 페널티 삭제 API 테스트 코드 작성
     #40
    
    * Feat: 페널티 삭제 API 테스트 코드 추가
    - deletePenalty 테스트 코드 수정
    - findByIdAndMemberId 테스트 코드 추가
     #40

--- src/main/java/kea/enter/enterbe/api/penalty/service/AdminPenaltyService.java
+++ src/main/java/kea/enter/enterbe/api/penalty/service/AdminPenaltyService.java
@@ -1,6 +1,7 @@
 package kea.enter.enterbe.api.penalty.service;
 
 import kea.enter.enterbe.api.penalty.controller.response.GetPenaltyListResponse;
+import kea.enter.enterbe.api.penalty.service.dto.DeletePenaltyServiceDto;
 import kea.enter.enterbe.api.penalty.service.dto.GetPenaltyListServiceDto;
 import kea.enter.enterbe.api.penalty.service.dto.PostPenaltyServiceDto;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 public interface AdminPenaltyService {
     void createPenalty(PostPenaltyServiceDto postPenaltyServiceDto);
-    List<GetPenaltyListResponse> getPenaltyList(GetPenaltyListServiceDto of);
+    List<GetPenaltyListResponse> getPenaltyList(GetPenaltyListServiceDto getPenaltyListServiceDto);
+    void deletePenalty(DeletePenaltyServiceDto deletePenaltyServiceDto);
 }
--- src/main/java/kea/enter/enterbe/domain/penalty/repository/PenaltyRepository.java
+++ src/main/java/kea/enter/enterbe/domain/penalty/repository/PenaltyRepository.java
@@ -5,9 +5,16 @@ import kea.enter.enterbe.domain.penalty.entity.PenaltyState;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PenaltyRepository extends JpaRepository<Penalty, Long> {
+    // 사용자의 페널티를 조회한다
+    Optional<Penalty> findByIdAndMemberIdAndState(Long penaltyId, Long memberId, PenaltyState state);
+
+    // 상태값 없이 사용자의 페널티를 조회한다
+    Optional<Penalty> findByIdAndMemberId(Long penaltyId, Long memberId);
+
     // 사용자의 페널티 목록을 조회한다
     List<Penalty> findAllByMemberIdAndStateOrderByCreatedAt(Long memberId, PenaltyState state);
 }
