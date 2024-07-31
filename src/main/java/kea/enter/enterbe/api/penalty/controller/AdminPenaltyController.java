63714a64774f3a7369fa717196ad7db746b19a38
정조은 <jjy020121@gmail.com>
2024-07-31 10:30:27 +0900

    빌드 오류 수정 (#46)
    * refactor: 빌드 오류 수정
    - AdminPenaltyServiceImpl -> AdminPenaltyService
     #36

--- src/main/java/kea/enter/enterbe/api/penalty/controller/AdminPenaltyController.java
+++ src/main/java/kea/enter/enterbe/api/penalty/controller/AdminPenaltyController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kea.enter.enterbe.api.penalty.controller.request.PostPenaltyRequest;
+import kea.enter.enterbe.api.penalty.service.AdminPenaltyService;
 import kea.enter.enterbe.api.penalty.service.AdminPenaltyServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ import static kea.enter.enterbe.global.common.api.CustomResponseCode.SUCCESS;
 @RequiredArgsConstructor
 @RequestMapping("/admin/penalties")
 public class AdminPenaltyController {
-    private final AdminPenaltyServiceImpl adminPenaltyService;
+    private final AdminPenaltyService adminPenaltyService;
 
     /* 페널티 부여 API */
     @Operation(summary = "사용자 페널티 부여 API", description = "사용자에게 페널티를 부여합니다.")
