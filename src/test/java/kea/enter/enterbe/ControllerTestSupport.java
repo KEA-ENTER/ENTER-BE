1646e2e2e380adb2f7c7b3119f00109e0c30c37b
정조은 <jjy020121@gmail.com>
2024-08-02 10:04:17 +0900

    인수 현황 조회 API 구현 (#69)
    * Feat: 인수 현황 조회 API
    - 인수 현황 조회 API
     #55
    
    * Feat: 인수 현황 조회 API 테스트 코드 작성
    - 인수 현황 조회 API 테스트 코드 작성
     #55

--- src/test/java/kea/enter/enterbe/ControllerTestSupport.java
+++ src/test/java/kea/enter/enterbe/ControllerTestSupport.java
@@ -1,13 +1,15 @@
 package kea.enter.enterbe;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kea.enter.enterbe.api.apply.controller.AdminApplyController;
 import kea.enter.enterbe.api.apply.service.AdminApplyService;
 import kea.enter.enterbe.api.question.controller.QuestionController;
 import kea.enter.enterbe.api.question.service.QuestionService;
 import kea.enter.enterbe.api.penalty.controller.AdminPenaltyController;
 import kea.enter.enterbe.api.penalty.service.AdminPenaltyService;
+import kea.enter.enterbe.api.take.controller.AdminTakeController;
+import kea.enter.enterbe.api.take.service.AdminTakeService;
 import kea.enter.enterbe.api.vehicle.controller.VehicleController;
-import kea.enter.enterbe.api.vehicle.service.AdminVehicleService;
 import kea.enter.enterbe.api.vehicle.service.VehicleService;
 import kea.enter.enterbe.global.security.SecurityConfig;
 import kea.enter.enterbe.global.util.FileUtil;
@@ -17,7 +19,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {
-    SecurityConfig.class, VehicleController.class, AdminPenaltyController.class, QuestionController.class
+    SecurityConfig.class, VehicleController.class, AdminPenaltyController.class, QuestionController.class,
+    AdminApplyController.class, AdminTakeController.class
 })
 public abstract class ControllerTestSupport {
 
@@ -35,5 +38,6 @@ public abstract class ControllerTestSupport {
     protected AdminPenaltyService adminPenaltyService;
     @MockBean
     protected AdminApplyService adminApplyService;
-
+    @MockBean
+    protected AdminTakeService adminTakeService;
 }
