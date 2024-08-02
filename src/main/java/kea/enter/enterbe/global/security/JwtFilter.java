f35aaa821f96da1c8d384736d058a67068b4a0fc
KimYoungWoo <120362572+KimuYounguWoo@users.noreply.github.com>
2024-08-02 13:16:37 +0900

    시큐리티 설정 (#67)
    스프링 시큐리티 등 인가에 필요한 라이브러리 설정 #27
    
    * Feat : 스프링 시큐리티 등 인가에 필요한 라이브러리 설정(#27)
    
    - controller를  제외한 로그인 기능 구현
    
    * Feat : 스프링 시큐리티 등 인가에 필요한 라이브러리 설정(#27)
    
    - 시큐리티 컨픽 설정
    
    스프링 시큐리티 등 인가에 필요한 라이브러리 설정 #27
    
    * Feat : 스프링 시큐리티 등 인가에 필요한 라이브러리 설정(#27)
    
    - 인메모리 유저 구현, 기타 무시 패턴 적용
    
    스프링 시큐리티 등 인가에 필요한 라이브러리 설정 #27
    
    * Fix : 스프링 시큐리티 등 인가에 필요한 라이브러리 설정(#27)
    
    - 코드 리뷰 피드백 적용
    
    스프링 시큐리티 등 인가에 필요한 라이브러리 설정 #27
    
    ---------
    
    Co-authored-by: Dongseok Kang <dsk08208@gmail.com>

---
+++ src/main/java/kea/enter/enterbe/global/security/JwtFilter.java
@@ -0,0 +1,45 @@
+package kea.enter.enterbe.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import kea.enter.enterbe.api.auth.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter { // OncePerRequestFilter -> 한 번 실행 보장
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        //JWT가 헤더에 있는 경우
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            if (jwtUtil.validateToken(token)) {
+                Long userId = jwtUtil.getUserId(token);
+
+                UserDetails userDetails = customUserDetailsService.loadUserByUsername(userId.toString());
+
+                if (userDetails != null) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response); // 다음 필터로 넘기기
+    }
+}
+
--- src/test/java/kea/enter/enterbe/ControllerTestSupport.java
+++ src/test/java/kea/enter/enterbe/ControllerTestSupport.java
@@ -1,6 +1,9 @@
 package kea.enter.enterbe;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kea.enter.enterbe.api.auth.controller.AuthController;
+import kea.enter.enterbe.api.auth.service.AuthService;
+import kea.enter.enterbe.api.auth.service.CustomUserDetailsService;
 import kea.enter.enterbe.api.apply.controller.AdminApplyController;
 import kea.enter.enterbe.api.apply.service.AdminApplyService;
 import kea.enter.enterbe.api.question.controller.QuestionController;
@@ -11,17 +14,20 @@ import kea.enter.enterbe.api.take.controller.AdminTakeController;
 import kea.enter.enterbe.api.take.service.AdminTakeService;
 import kea.enter.enterbe.api.vehicle.controller.VehicleController;
 import kea.enter.enterbe.api.vehicle.service.VehicleService;
+import kea.enter.enterbe.global.security.JwtUtil;
 import kea.enter.enterbe.global.security.SecurityConfig;
 import kea.enter.enterbe.global.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {
-    SecurityConfig.class, VehicleController.class, AdminPenaltyController.class, QuestionController.class,
+  VehicleController.class, AdminPenaltyController.class, QuestionController.class,
     AdminApplyController.class, AdminTakeController.class
 })
+@Import({SecurityConfig.class})
 public abstract class ControllerTestSupport {
 
     @Autowired
@@ -29,13 +35,17 @@ public abstract class ControllerTestSupport {
     @Autowired
     protected ObjectMapper objectMapper;
     @MockBean
-    protected QuestionService questionService;
-    @MockBean
     protected VehicleService vehicleService;
     @MockBean
     protected FileUtil fileUtil;
     @MockBean
     protected AdminPenaltyService adminPenaltyService;
+    @MockBean
+    protected AuthService authService;
+    @MockBean
+    protected CustomUserDetailsService customUserDetailsService;
+    @MockBean
+    protected JwtUtil jwtUtil;
     @MockBean
     protected AdminApplyService adminApplyService;
     @MockBean
