fbe618f76fc55810e53d083424669d24e18ab25c
joeun-01 <jjy020121@gmail.com>
2024-08-06 21:39:19 +0900

    Feat: 신청 내역 목록 조회 API - Apply Entity 신청 목적 추가 - 신청 내역 목록 조회 API  #127

---
+++ src/main/java/kea/enter/enterbe/api/lottery/controller/dto/request/ApplicantSearchType.java
@@ -0,0 +1,5 @@
+package kea.enter.enterbe.api.lottery.controller.dto.request;
+
+public enum ApplicantSearchType {
+    ALL, ID, NAME
+}
---
+++ src/main/java/kea/enter/enterbe/domain/apply/entity/ApplyPurpose.java
@@ -0,0 +1,5 @@
+package kea.enter.enterbe.domain.apply.entity;
+
+public enum ApplyPurpose {
+    TRAVEL, EVENT, HOBBY, SELF, EDUCATION
+}
