660d135db1e312bbebd1124aa1db3854764172fb
joeun-01 <jjy020121@gmail.com>
2024-08-08 18:42:09 +0900

    Feat: 차량 반납 보고서 상세 조회 API - 차량 반납 보고서 상세 조회 API  #161

--- src/main/java/kea/enter/enterbe/api/take/service/AdminTakeService.java
+++ src/main/java/kea/enter/enterbe/api/take/service/AdminTakeService.java
@@ -1,8 +1,11 @@
 package kea.enter.enterbe.api.take.service;
 
+import kea.enter.enterbe.api.take.controller.dto.response.GetReturnReportResponse;
 import kea.enter.enterbe.api.take.controller.dto.response.GetTakeSituationResponse;
+import kea.enter.enterbe.api.take.service.dto.GetReturnReportServiceDto;
 import kea.enter.enterbe.api.take.service.dto.GetTakeSituationServiceDto;
 
 public interface AdminTakeService {
     GetTakeSituationResponse getTakeSituation(GetTakeSituationServiceDto dto);
+    GetReturnReportResponse getReturnReport(GetReturnReportServiceDto dto);
 }
