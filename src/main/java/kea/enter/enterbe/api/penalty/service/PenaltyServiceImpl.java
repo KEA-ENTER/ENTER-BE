0c8e52e636747740f5ca655aaca8eb1981e54aa5
SeohyunSim8 <128570477+SeohyunSim8@users.noreply.github.com>
2024-08-14 17:19:34 +0900

    Feat: 페이징 기능 추가 (#277)
    * Feat: 페이징 기능 추가
    - 페이징 기능 추가
    #144

--- src/main/java/kea/enter/enterbe/api/penalty/service/PenaltyServiceImpl.java
+++ src/main/java/kea/enter/enterbe/api/penalty/service/PenaltyServiceImpl.java
@@ -1,24 +1,22 @@
 package kea.enter.enterbe.api.penalty.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import io.swagger.v3.oas.annotations.media.Schema;
 import kea.enter.enterbe.api.penalty.controller.dto.response.GetPenaltyListResponse;
 import kea.enter.enterbe.api.penalty.controller.dto.response.GetPenaltyResponse;
+import kea.enter.enterbe.api.penalty.controller.dto.response.PenaltyInfo;
 import kea.enter.enterbe.api.penalty.service.dto.GetPenaltyListServiceDto;
 import kea.enter.enterbe.api.penalty.service.dto.GetPenaltyServiceDto;
 import kea.enter.enterbe.domain.member.entity.MemberState;
 import kea.enter.enterbe.domain.member.repository.MemberRepository;
 import kea.enter.enterbe.domain.penalty.entity.Penalty;
 import kea.enter.enterbe.domain.penalty.entity.PenaltyLevel;
-import kea.enter.enterbe.domain.penalty.entity.PenaltyReason;
 import kea.enter.enterbe.domain.penalty.entity.PenaltyState;
 import kea.enter.enterbe.domain.penalty.repository.PenaltyRepository;
 import kea.enter.enterbe.global.common.exception.CustomException;
 import kea.enter.enterbe.global.common.exception.ResponseCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,25 +28,23 @@ public class PenaltyServiceImpl implements PenaltyService {
     private final MemberRepository memberRepository;
     private final PenaltyRepository penaltyRepository;
 
-    public List<GetPenaltyListResponse> getPenaltyList(GetPenaltyListServiceDto dto) {
+    public GetPenaltyListResponse getPenaltyList(GetPenaltyListServiceDto dto) {
         memberRepository.findByIdAndState(dto.getMemberId(), MemberState.ACTIVE)
             .orElseThrow(() -> new CustomException(ResponseCode.MEMBER_NOT_FOUND));
 
-        List<Penalty> penaltyList = penaltyRepository.findAllByMemberIdAndStateOrderByCreatedAt(dto.getMemberId(), PenaltyState.ACTIVE);
+        Page<Penalty> penaltyPage = penaltyRepository.findAllByMemberIdAndStateOrderByCreatedAt(dto.getMemberId(), PenaltyState.ACTIVE, dto.getPageable());
+        List<PenaltyInfo> penaltyInfoList = penaltyPage.stream()
+            .map(d -> PenaltyInfo.of(d.getId(), d.getReason().toString(), d.getLevel().toString(), d.getCreatedAt().toString()))
+            .toList();
 
-        List<GetPenaltyListResponse> responses = new ArrayList<>();
-
-        for (Penalty penalty : penaltyList) {
-
-            responses.add(GetPenaltyListResponse.builder()
-                    .penaltyId(penalty.getId())
-                    .reason(penalty.map(penalty.getReason()))
-                    .level(PenaltyLevel.getDuration(penalty.getLevel()))
-                    .createdAt(penalty.getCreatedAt().toLocalDate().toString())
-                .build());
-        }
-
-        return responses;
+        return GetPenaltyListResponse.of(
+            penaltyInfoList,
+            penaltyPage.getNumber(),
+            penaltyPage.getSize(),
+            penaltyPage.getTotalElements(),
+            penaltyPage.getTotalPages(),
+            penaltyPage.hasNext()
+        );
     }
 
     public GetPenaltyResponse getPenalty(GetPenaltyServiceDto dto) {
