1b3d1eae379a2783b06328ff6e3af7eca97de1b9
정조은 <jjy020121@gmail.com>
2024-08-16 17:26:53 +0900

    Feat: 신청 내역 목록 조회 API 응답 수정 (#306)
    - 신청 내역 목록 조회 API 응답 수정

--- src/main/java/kea/enter/enterbe/api/lottery/controller/dto/response/GetApplicantListResponse.java
+++ src/main/java/kea/enter/enterbe/api/lottery/controller/dto/response/GetApplicantListResponse.java
@@ -10,20 +10,44 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class GetApplicantListResponse {
+    @Schema(description = "회차", example = "24")
+    private int round;
+
+    @Schema(description = "인수 날짜", example = "2024-08-03")
+    private String takeDate;
+
+    @Schema(description = "차량 모델", example = "G80")
+    private String vehicleModel;
+
+    @Schema(description = "차량 번호", example = "123가 4568")
+    private String vehicleNo;
+
     private List<ApplicantInfo> applicantList;
+
     @Schema(description = "현재 페이지", example = "0")
     private int page;
+
     @Schema(description = "현재 페이지 사이즈", example = "8")
     private int size;
+
     @Schema(description = "전체 개수", example = "20")
     private long totalElements;
+
     @Schema(description = "전체 페이지", example = "3")
     private int totalPages;
+
     @Schema(description = "다음 페이지 존재 여부", example = "true")
     private boolean hasNextPage;
 
     @Builder
-    public GetApplicantListResponse(List<ApplicantInfo> applicantList, int page, int size, long totalElements, int totalPages, boolean hasNextPage) {
+    public GetApplicantListResponse(
+        int round, String takeDate, String vehicleModel, String vehicleNo,
+        List<ApplicantInfo> applicantList,
+        int page, int size, long totalElements, int totalPages, boolean hasNextPage) {
+        this.round = round;
+        this.takeDate = takeDate;
+        this.vehicleModel = vehicleModel;
+        this.vehicleNo = vehicleNo;
         this.applicantList = applicantList;
         this.page = page;
         this.size = size;
@@ -32,8 +56,15 @@ public class GetApplicantListResponse {
         this.hasNextPage = hasNextPage;
     }
 
-    public static GetApplicantListResponse of(List<ApplicantInfo> applicantList, int page, int size, long totalElements, int totalPages, boolean hasNextPage) {
+    public static GetApplicantListResponse of(
+        int round, String takeDate, String vehicleModel, String vehicleNo,
+        List<ApplicantInfo> applicantList,
+        int page, int size, long totalElements, int totalPages, boolean hasNextPage) {
         return GetApplicantListResponse.builder()
+            .round(round)
+            .takeDate(takeDate)
+            .vehicleModel(vehicleModel)
+            .vehicleNo(vehicleNo)
             .applicantList(applicantList)
             .page(page)
             .size(size)
