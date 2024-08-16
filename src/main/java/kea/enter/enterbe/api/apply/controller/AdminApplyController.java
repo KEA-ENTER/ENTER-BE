39fe15dfdd1cb8c8cb595842854ad09724994ebc
joeun-01 <jjy020121@gmail.com>
2024-08-16 18:44:28 +0900

    Docs: 기존 API 스웨거 형식 정리 (#307) - 성공 응답 추가

--- src/main/java/kea/enter/enterbe/api/apply/controller/AdminApplyController.java
+++ src/main/java/kea/enter/enterbe/api/apply/controller/AdminApplyController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kea.enter.enterbe.api.apply.controller.dto.response.GetApplySituationResponse;
 import kea.enter.enterbe.api.apply.service.AdminApplyService;
 import kea.enter.enterbe.api.apply.service.dto.GetApplySituationServiceDto;
+import kea.enter.enterbe.api.lottery.controller.dto.response.GetApplicantListResponse;
 import kea.enter.enterbe.global.common.exception.ErrorResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,7 @@ public class AdminApplyController {
     /* 응모 현황 조회 API */
     @Operation(summary = "응모 현황 조회 API", description = "현재 회차 응모 현황을 조회합니다.")
     @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = GetApplySituationResponse.class))),
         @ApiResponse(responseCode = "AUT-ERR-010", description = "인증되지 않은 사용자입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "APR-ERR-001", description = "신청 회차를 찾을 수 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "GLB-ERR-003", description = "내부 서버 오류입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
--- src/main/java/kea/enter/enterbe/api/penalty/controller/AdminPenaltyController.java
+++ src/main/java/kea/enter/enterbe/api/penalty/controller/AdminPenaltyController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import kea.enter.enterbe.api.lottery.controller.dto.response.GetLotteryListResponse;
 import kea.enter.enterbe.api.penalty.controller.dto.request.PostAdminPenaltyRequest;
 import kea.enter.enterbe.api.penalty.controller.dto.response.GetAdminPenaltyListResponse;
 import kea.enter.enterbe.api.penalty.service.AdminPenaltyService;
@@ -38,6 +39,7 @@ public class AdminPenaltyController {
     /* 페널티 부여 API */
     @Operation(summary = "사용자 페널티 부여 API", description = "사용자에게 페널티를 부여합니다.")
     @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
         @ApiResponse(responseCode = "AUT-ERR-010", description = "인증되지 않은 사용자입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "MEM-ERR-001", description = "멤버를 찾을 수 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "GLB-ERR-003", description = "내부 서버 오류입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
@@ -53,6 +55,7 @@ public class AdminPenaltyController {
     /* 페널티 목록 조회 API */
     @Operation(summary = "사용자 페널티 목록 조회 API", description = "사용자의 페널티 목록을 조회합니다.")
     @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = GetAdminPenaltyListResponse.class))),
         @ApiResponse(responseCode = "AUT-ERR-010", description = "인증되지 않은 사용자입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "MEM-ERR-001", description = "멤버를 찾을 수 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "GLB-ERR-003", description = "내부 서버 오류입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
@@ -66,6 +69,7 @@ public class AdminPenaltyController {
     /* 페널티 삭제 API */
     @Operation(summary = "사용자 페널티 삭제 API", description = "사용자의 페널티를 삭제합니다.")
     @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
         @ApiResponse(responseCode = "AUT-ERR-010", description = "인증되지 않은 사용자입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "MEM-ERR-001", description = "멤버를 찾을 수 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "PEN-ERR-001", description = "페널티를 찾을 수 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
