696e1a3d297fac912f1fcd45f9c6f24519478982
wcorn <dsk08208@gmail.com>
2024-08-17 19:44:37 +0900

    Docs: 최근 5회차 평균 경쟁률 response swagger 추가
    response swagger 추가
     #313

--- src/main/java/kea/enter/enterbe/api/lottery/controller/dto/response/GetRecentCompetitionRateResponse.java
+++ src/main/java/kea/enter/enterbe/api/lottery/controller/dto/response/GetRecentCompetitionRateResponse.java
@@ -1,12 +1,14 @@
 package kea.enter.enterbe.api.lottery.controller.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class GetRecentCompetitionRateResponse {
-
+    @Schema(description = "회차", example = "11")
     private Integer round;
+    @Schema(description = "경쟁률 (51 이라고 반환 되면 51:1이라고 생각하시면 됩니다)", example = "51")
     private String competitionRate;
 
     @Builder
