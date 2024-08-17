556ff853ea5b94d91a27c2a30bedb79a0a8e2639
wcorn <dsk08208@gmail.com>
2024-08-17 20:01:55 +0900

    Docs: 최근 5회차 평균 빠진 대기번호 swagger 수정
    response swagger 예시 값 및 설명 추가
     #313

--- src/main/java/kea/enter/enterbe/api/lottery/controller/dto/response/GetRecentWaitingAverageNumbersResponse.java
+++ src/main/java/kea/enter/enterbe/api/lottery/controller/dto/response/GetRecentWaitingAverageNumbersResponse.java
@@ -1,12 +1,15 @@
 package kea.enter.enterbe.api.lottery.controller.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class GetRecentWaitingAverageNumbersResponse {
-
+    @Schema(description = "회차", example = "11")
     private Integer round;
+
+    @Schema(description = "대기번호 빠짐 수(0이면 처음 당첨된 사람이 가져간것, 나머지는 1,2,3...)", example = "14")
     private String WaitingAverageNumber;
 
     @Builder
