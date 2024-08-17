f2343d2594288f6bdcde3a8821306ad2241c0ef2
wcorn <dsk08208@gmail.com>
2024-08-17 19:41:41 +0900

    Docs: 어떤 보고서 제출  종류 반환 swagger 추가
    swagger 보고서 response 추가
     #313

--- src/main/java/kea/enter/enterbe/api/member/controller/dto/response/GetMemberPostReportTypeResponse.java
+++ src/main/java/kea/enter/enterbe/api/member/controller/dto/response/GetMemberPostReportTypeResponse.java
@@ -1,11 +1,12 @@
 package kea.enter.enterbe.api.member.controller.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class GetMemberPostReportTypeResponse {
-
+    @Schema(description = "보고서 종류 반환 (TAKE, RETURN. NONE)",example = "TAKE")
     private String reportType;
 
     @Builder
