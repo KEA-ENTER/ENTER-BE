442a3b8efb66fe81ba92cf505c48c6c5f059fe80
doromoro <khan1148943@gmail.com>
2024-08-18 22:49:40 +0900

    Docs : MemberContoller - getUserRoutingInformation 스웨거 코드 추가 (#337)
    - MemberController : Operation수정, ApiResponse추가
    - GetRoutingResponse : 스키마 추가

--- src/main/java/kea/enter/enterbe/api/member/controller/MemberController.java
+++ src/main/java/kea/enter/enterbe/api/member/controller/MemberController.java
@@ -1,11 +1,16 @@
 package kea.enter.enterbe.api.member.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kea.enter.enterbe.api.member.controller.dto.response.GetMemberPostReportTypeResponse;
 import kea.enter.enterbe.api.member.controller.dto.response.GetMemberScoreResponse;
 import kea.enter.enterbe.api.member.controller.dto.response.GetRoutingResponse;
 import kea.enter.enterbe.api.member.service.MemberService;
+import kea.enter.enterbe.global.common.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -36,7 +41,19 @@ public class MemberController {
         return ResponseEntity.ok(memberService.getMemberPostReportType(memberId));
     }
 
-    @Operation(summary = "사용자 라우팅 정보 조회 (차량 신청 메뉴에 접근했을 때 라우팅)")
+    @Operation(
+        summary = "사용자 라우팅 정보 조회 (차량 신청 메뉴에 접근했을 때 라우팅)",
+        description = "로그인이 성공한 사용자의 상태(사원, 신청자, 당첨자, 대기자, 미당첨자)와 기간(신청서 작성 기간, 추첨 대기 기간, 결과 발표 기간, 회차 대기 기간)에 따른 "
+            + "페이지 정보와 사용자의 상태를 제공하는 API"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "라우팅 페이지 정보와 사용자의 상태 값",
+            content = @Content(schema = @Schema(implementation = GetRoutingResponse.class), mediaType = "application/json")),
+        @ApiResponse(responseCode = "400", description = "올바르지 않은 시간 값이 들어온 경우", content = @Content(schema = @Schema(implementation = CustomException.class))),
+        @ApiResponse(responseCode = "404", description = "라우팅 정보를 제공할 수 없는 경우", content = @Content(schema = @Schema(implementation = CustomException.class)))
+    })
     @GetMapping(value = "/routing")
     public ResponseEntity<GetRoutingResponse> getUserRoutingInformation(Authentication authentication) {
         Long memberId = Long.valueOf(authentication.getName());
