867829a38f5a7deeee200048414fdce01ba9493a
doromoro <khan1148943@gmail.com>
2024-08-19 16:59:31 +0900

    Feat : 패널티 제한 여부를 확인하는 API (#344) - 실행 테스트(성공 목록) 1. 현재 시간이 패널티 기간에 해당할 때 2. 블랙리스트는 9999년 제한 3. 패널티가 없는 경우 - 테스트 더 필요한 목록 1. 패널티가 중첩적으로 존재하는 경우 2. 패널티가 존재하지만 현재 기간 이전에 만료된 경우 - 빌드 성공

--- src/main/java/kea/enter/enterbe/api/penalty/controller/dto/response/GetMemberInPenaltyPeriodResponse.java
+++ src/main/java/kea/enter/enterbe/api/penalty/controller/dto/response/GetMemberInPenaltyPeriodResponse.java
@@ -3,29 +3,31 @@ package kea.enter.enterbe.api.penalty.controller.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.time.Period;
 
 @Getter
 @NoArgsConstructor
 public class GetMemberInPenaltyPeriodResponse {
 
     private boolean isInProgress;
-    private Period restOfPenalties;
-    private String message;
+    private int year;
+    private int month;
+    private int day;
 
     @Builder
-    public GetMemberInPenaltyPeriodResponse(boolean isInProgress, Period restOfPenalties,
-        String message) {
+    public GetMemberInPenaltyPeriodResponse(boolean isInProgress, int year, int month, int day) {
         this.isInProgress = isInProgress;
-        this.restOfPenalties = restOfPenalties;
-        this.message = message;
+        this.year = year;
+        this.month = month;
+        this.day = day;
     }
 
-    public static GetMemberInPenaltyPeriodResponse of(boolean isInProgress, Period restOfPenalties,
-        String message) {
+    public static GetMemberInPenaltyPeriodResponse of(boolean isInProgress, int year, int month, int day) {
         return GetMemberInPenaltyPeriodResponse.builder()
             .isInProgress(isInProgress)
-            .restOfPenalties(restOfPenalties)
-            .message(message).build();
+            .year(year)
+            .month(month)
+            .day(day)
+            .build();
     }
+
 }
--- src/main/java/kea/enter/enterbe/api/penalty/service/PenaltyServiceImpl.java
+++ src/main/java/kea/enter/enterbe/api/penalty/service/PenaltyServiceImpl.java
@@ -1,5 +1,9 @@
 package kea.enter.enterbe.api.penalty.service;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import kea.enter.enterbe.api.penalty.controller.dto.response.GetMemberInPenaltyPeriodResponse;
 import kea.enter.enterbe.api.penalty.controller.dto.response.GetPenaltyListResponse;
@@ -66,6 +70,67 @@ public class PenaltyServiceImpl implements PenaltyService {
 
     @Override
     public GetMemberInPenaltyPeriodResponse getMemberInPenaltyPeriod(Long memberId) {
-        return null;
+        // 사용자의 패널티 내역 조회
+        List<Penalty> penaltyList = penaltyRepository.findAllByMemberIdAndStateOrderByCreatedAt(memberId, PenaltyState.ACTIVE);
+        // 패널티가 없는 경우
+        if(penaltyList.isEmpty()){
+            return GetMemberInPenaltyPeriodResponse.of(false, 0,0,0);
+        }
+        // 패널티가 유지되는 마지막 시간을 먼저 현재 시간으로 선언
+        LocalDate penaltyEnd = LocalDate.now();
+        // 현재 패널티 제한을 받는지 여부를 담는 변수, 초기값은 false
+        boolean inProgress = false;
+
+        // 패널티 생성일자를 담는 스택
+        Deque<LocalDate> createdAtStack = new LinkedList<>();
+        // 기간 자체에 대한 스택
+        Deque<Long> periodStack = new LinkedList<>();
+        // 패널티 종료일을 쌓는 스택 -> 솔직히 스택 안써도 될 것 같긴한데 그냥 변수를 쓰기가 싫어
+        Deque<LocalDate> endStack = new LinkedList<>();
+
+        // 각 패널티를 조회하며 연산
+        for(Penalty penalty : penaltyList) {
+            // 블랙리스트를 보게 되면 연산 종료 및 return 할 수 있도록 진행 (뭔가 변수가 필요할 듯)
+            if(penalty.getLevel().equals(PenaltyLevel.BLACKLIST)){
+                return GetMemberInPenaltyPeriodResponse.of(true, 9999, 12,30);
+            }
+
+            LocalDate createdAt = penalty.getCreatedAt().toLocalDate();
+            createdAtStack.push(createdAt);
+            Long period = penaltyConvertToPeriod(penalty.getLevel());
+            periodStack.push(period);
+            // 이전 createdAtStack과 비교했을 때 넘어선다면 이전 것을 기준으로 패널티 덧샘 시작
+            if(createdAtStack.size()>=2 && !endStack.isEmpty() && endStack.peek().isAfter(createdAtStack.peek())){
+                createdAtStack.pop();
+                LocalDate periodOfEnd = createdAtStack.peek();
+                while(!periodStack.isEmpty()){
+                    periodOfEnd = periodOfEnd.plusWeeks(periodStack.pop());
+                }
+                endStack.push(periodOfEnd);
+            }else {
+                LocalDate periodOfEnd = createdAtStack.peek().plusWeeks(periodStack.peek());
+                endStack.push(periodOfEnd);
+            }
+
+
+            // 현재 값보다 넘어가는 경우를 확인
+            if(endStack.peek().isAfter(penaltyEnd) || endStack.peek().isEqual(penaltyEnd)){
+                penaltyEnd = endStack.peek();
+                inProgress = true;
+            }
+
+        }
+        Period restOfPenalties = Period.between(LocalDate.now(), penaltyEnd);
+        return GetMemberInPenaltyPeriodResponse.of(inProgress, restOfPenalties.getYears(), restOfPenalties.getMonths(), restOfPenalties.getDays());
+    }
+
+    private Long penaltyConvertToPeriod(PenaltyLevel level){
+        return switch (level) {
+            case MINIMUM -> 2L;
+            case LOW -> 4L;
+            case MEDIUM ->16L;
+            case HIGH -> 64L;
+            case BLACKLIST -> 99999L;
+        };
     }
 }
