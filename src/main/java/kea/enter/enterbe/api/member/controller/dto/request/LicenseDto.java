1cf6dd050f3f0e47e82207f006322dc481dce0b5
doromoro <57747682+doromoro@users.noreply.github.com>
2024-08-19 09:28:28 +0900

    Docs : Member(routing), License 스웨거 코드 추가 (#337)
    - Member(Routing)
    - Licesnse

--- src/main/java/kea/enter/enterbe/api/member/controller/dto/request/LicenseDto.java
+++ src/main/java/kea/enter/enterbe/api/member/controller/dto/request/LicenseDto.java
@@ -1,5 +1,6 @@
 package kea.enter.enterbe.api.member.controller.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -14,14 +15,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LicenseDto {
 
+    @Schema(description = "사용자 면허 일련번호", example = "123456789012")
     @NotBlank
     @Pattern(regexp = "\\d{12}", message = "12자리의 숫자만 작성되어야 합니다.")
     private String licenseId;
 
+    @Schema(description = "사용저 암호 일련번호(면허증 우측 하단 사진 위)", example = "123ABC")
     @NotBlank
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{5,6}$", message = "암호 일련번호는 영문, 숫자를 포함한 5~6자리 텍스트입니다.")
     private String licensePassword;
 
+    @Schema(description = "개인정보 처리 방침 동의 여부", example = "true")
     @NotNull
     @AssertTrue(message = "개인 정보처리 방침에 동의해야 합니다.")
     private Boolean isAgreeTerms;
--- src/main/java/kea/enter/enterbe/api/member/controller/dto/response/GetRoutingResponse.java
+++ src/main/java/kea/enter/enterbe/api/member/controller/dto/response/GetRoutingResponse.java
@@ -1,12 +1,15 @@
 package kea.enter.enterbe.api.member.controller.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class GetRoutingResponse {
 
+    @Schema(description = "신청메뉴 라우팅 페이지 번호(1:신청서 작성 페이지 / 2: 신청서 내역 조회 페이지 / 3: 신청 기간 아니에요 페이지 / 4: 추첨 결과 확인 페이지)", example = "1")
     private Integer routingId;
+    @Schema(description = "사용자의 상태 : EMPLOYEE(사원), APPLICANT(신청자), WINNER(당첨자), CANDIDATE(대기자), NON_WINNER(미당첨자)", example = "EMPLOYEE")
     private String userState;
 
     @Builder
